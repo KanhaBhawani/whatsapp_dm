2a82cbcc56d9fb948dcb1b2b62ff891313e2164b
Kanha Bhawani <ghanishtsinghal@gmail.com>
2022-06-26 16:09:48 +0530

    removing bugs

--- script/index.js
+++ script/index.js
@@ -1,4 +1,9 @@
-function myFunc(){
+
+
+document.getElementById("submitButton").addEventListener("click", ()=>{
     var phoneNumber = document.getElementById("phn").value;
-    localStorage.setItem('phoneNo', phoneNumber);
-};
+    document.getElementById("otp_div").classList.remove("hidden");
+    document.getElementById("otp_p").innerText = "Chat on WhatsApp with +91 " + phoneNumber;
+    document.getElementById("otp").href = "https://wa.me/+91" + phoneNumber;
+    document.getElementById("otp").innerText = "Continue to Chat";
+});
--- script/link.js
+++
@@ -1,4 +0,0 @@
-var phn = localStorage.getItem("phoneNo");
-document.getElementById("otp").href = "https://wa.me/+91" + phn;
-document.getElementById("otp_p").innerText = "Chat on WhatsApp with +91 " + phn;
-window.location.reload;
