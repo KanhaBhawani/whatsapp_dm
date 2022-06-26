840d02458e0bd5ee91cd004230e67582b1a7fe34
Kanha Bhawani <ghanishtsinghal@gmail.com>
2022-06-26 15:02:56 +0530

    solving some bugs

--- script/link.js
+++ script/link.js
@@ -1,3 +1,4 @@
 var phn = localStorage.getItem("phoneNo");
 document.getElementById("otp").href = "https://wa.me/+91" + phn;
 document.getElementById("otp_p").innerText = "Chat on WhatsApp with +91 " + phn;
+window.location.reload;
