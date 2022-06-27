d7ded92b13b5fccce014ab9cb52b42d8451fedad
Kanha Bhawani <ghanishtsinghal@gmail.com>
2022-06-27 15:41:27 +0530

    adding QR code scanner 1

--- script/index.js
+++ script/index.js
@@ -6,5 +6,6 @@ document.getElementById("submitButton").addEventListener("click", ()=>{
     document.getElementById("otp_p").innerText = "Chat on WhatsApp with +91 " + phoneNumber;
     document.getElementById("otp").href = "https://wa.me/+91" + phoneNumber;
     document.getElementById("otp").innerText = "Continue to Chat";
+    document.getElementById("QrCode").innerHTML = "";
     new QRCode(document.getElementById("QrCode"), "https://wa.me/+91" + phoneNumber);
 });
